9d2324e7d547cd6a78a7cffa0ea21db1991eeafd
Marek Ozimina <marek.ozimina@yahoo.com>
2025-03-14 14:24:06 +0100

    Dodano funkcje actual_time

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import time
 
+class Waiting_time:
+    def __init__(self, time, type):
+        self.time = time
+        self.type = type
+
+    def actual_time(sel):
+        return time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
+
+    def waiting(self):
+        pass
+
+    def ringing(self):
+        pass
+
+if __name__ == "__main__":
+    przerwa = Waiting_time(10, 'przerwa')
+    print(przerwa.actual_time())
