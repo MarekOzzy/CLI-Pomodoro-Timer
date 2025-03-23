a1a96938479c3475234b3f07dcffa887f348052c
Marek Ozimina <marek.ozimina@yahoo.com>
2025-03-23 12:01:17 +0100

    Utworzono ekran startu

--- CUI.py
+++ CUI.py
@@ -10,48 +10,35 @@ from rich.live import Live
 class Interface:
     def __init__(self):
         self.console = Console()
-        self.layout = Layout()
         self.live = Live(refresh_per_second=5, screen=True)
 
-    def create_panel(self, content, type):
-        if type == 'start':
-            return Panel(
-                content,
-                title="Pomodoro Timer",
-                #title_align="right",
-                border_style="green on red",
-                expand=True,
-                height=self.console.height,
-                width=self.console.width
-            )  
-      
     def start(self, focus_time=None, break_time=None):
         """
         Rozpoczyna sesję Pomodoro.
         Naciśnij enter aby rozpocząć, lub podaj parametry jeśli nie podano ich jako flagę.
         Podaj długość czasu skupienia i przerwy jeśli nie zostały podane.
         """
-        start_layout = Layout()
 
-        panel = Panel(
-                'Hello to CLI Pomodoro timer',
-                title="Pomodoro Timer",
-                title_align="right",
-                border_style="#2dff32 on red",
-                expand=True,
-                height=self.console.height,
-                width=self.console.width
-            )
-        
+
+        start_layout = Layout()
         start_layout.split_column(
             Layout(Align.center("[purple]WELCOME TO CLI POMODORO TIMER![/]", vertical="bottom")),
             Layout(Align.center("[purple]PRESS ENTER TO START[/]", vertical="top"))
         )
 
+        start_panel = Panel(
+                start_layout,
+                title="Pomodoro Timer",
+                title_align="right",
+                border_style="green on red",
+                expand=True,
+                height=self.console.height,
+                width=self.console.width
+            )  
+
         with self.live:
-            ## OGARNĄĆ JAK PRZEKAZAĆ LAYOUT DO LIVE.UPDATE
-            self.live.update(panel) 
-            input()  # Wait for Enter key 
+            self.live.update(start_panel) 
+            input()  
 
         return focus_time, break_time
 
